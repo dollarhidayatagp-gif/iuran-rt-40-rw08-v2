ee3457e242f57df97673fe2816fed2b1a7b42980
dollarhidayatagp-gif <dollar.hidayatagp@gmail.com>
2026-07-20 06:16:43 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  output: 'export', // PENTING: Mengubah build menjadi HTML/CSS statis
+  basePath: '/iuran-rt-40-rw08-v2', // Ganti dengan nama repositori kamu
+  assetPrefix: '/iuran-rt-40-rw08-v2/', 
+  images: {
+    unoptimized: true, // PENTING: GitHub Pages tidak mendukung optimasi gambar bawaan Next.js
   },
 };
 
