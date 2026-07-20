1f144c3188a7f495def4bb60bd843b5d59964e59
dollarhidayatagp-gif <dollar.hidayatagp@gmail.com>
2026-07-20 21:33:08 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', 
-  basePath: '/iuran-rt-40-rw08-v2', 
-  assetPrefix: '/iuran-rt-40-rw08-v2/', 
   images: {
     unoptimized: true, 
   },
-  // TAMBAHKAN DUA BAGIAN INI:
   eslint: {
-    ignoreDuringBuilds: true, // Mengabaikan error eslint saat build
+    ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true, // Mengabaikan error TypeScript saat build
+    ignoreBuildErrors: true,
   },
 };
 
